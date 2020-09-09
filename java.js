47cecc3e853c4dc9aeb62e38c3ab68bfc41fcd23
Serhii Voytov <s.voytov2202@gmail.com>
2020-09-09 18:42:42 +0300

    Исправил ошибки

--- java.js
+++ java.js
@@ -1,21 +1,29 @@
-let search = new URLSearchParams(window.location.search);
-//if (search.has('username')) {}
+let search = new URLSearchParams(window.location.search); 
   let userName = search.get('username');
-  console.log(userName);
-  fetch('https://api.github.com/users/'+ userName)
-    .then(res => res.json())
-    .then(json => {
-      if (!json.bio) {json.bio= 'Пользователь не указал био'};
-      if (!json.name) {json.name= 'Пользователь не указал имя'};
-      document.body.innerHTML=`
-        <img src= ${json.avatar_url} alt="pic">
-        <h1>
-          <a href= ${json.html_url}>
-            ${json.name}
-          </a>
-        </h1>
-        <p>
-          ${json.bio}
-        </p>
-      `;
-    });   
+  if (search.has('username')) {
+    fetch('https://api.github.com/users/'+ userName)
+      .then(res => res.json())
+      .then(json => {
+         
+        if (!json.bio) {
+          json.bio= 'Пользователь не указал био';
+        }
+        
+        if (!json.name) {
+          json.name= 'Пользователь не указал имя';
+        }
+        document.body.innerHTML=`
+          <img src= ${json.avatar_url} alt="pic">
+          <h1>
+            <a href= ${json.html_url}>
+              ${json.name}
+            </a>
+          </h1>
+          <p>
+            ${json.bio}
+          </p>
+        `;
+      });   
+  } else {
+    document.body.innerHTML= 'Введите логин пользователя'; 
+  }

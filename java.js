69e97a8a3f6b8f0bf649df5826140397bd03c106
Serhii Voytov <s.voytov2202@gmail.com>
2020-09-10 20:55:38 +0300

    js

--- java.js
+++ java.js
@@ -1,29 +1,35 @@
-let search = new URLSearchParams(window.location.search); 
-  let userName = search.get('username');
-  if (search.has('username')) {
+let search = window.location.search;
+
+   if (search) {
+    userName = window.location.search.split('=')[1];
+   } else {
+     userName = 'voytov93';
+   }
     fetch('https://api.github.com/users/'+ userName)
       .then(res => res.json())
       .then(json => {
-         
-        if (!json.bio) {
-          json.bio= 'Пользователь не указал био';
-        }
-        
-        if (!json.name) {
-          json.name= 'Пользователь не указал имя';
-        }
-        document.body.innerHTML=`
-          <img src= ${json.avatar_url} alt="pic">
-          <h1>
-            <a href= ${json.html_url}>
-              ${json.name}
-            </a>
-          </h1>
-          <p>
-            ${json.bio}
-          </p>
-        `;
-      });   
-  } else {
-    document.body.innerHTML= 'Введите логин пользователя'; 
-  }
+
+        if (json.login) {
+
+          if (!json.bio) {
+            json.bio= 'Пользователь не указал био';
+          }
+
+          if (!json.name) {
+            json.name= 'Пользователь не указал имя';
+          }
+          document.body.innerHTML=`
+            <img src= ${json.avatar_url} alt="pic">
+            <h1>
+              <a href= ${json.html_url}>
+                ${json.name}
+              </a>
+            </h1>
+            <p>
+              ${json.bio}
+            </p>
+          `;
+      } else {
+        alert('Пользователь не найден')
+      }
+    });
